51ca7f204b2fa2272012f2d68a2d39f7234929da
Marcus Hammarberg <marcusoft.net@gmail.com>
2015-01-28 10:38:30 +0700

    First version of API done

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-var app = require("koa")();
+var app = module.exports = require("koa")();
 var route = require("koa-route");
 
-// routes
-app.use(route.get("/", function *(){ this.body = "Yup, it's working!"}));
+// api routes
+app.use(route.get("/api/sound/:filename" , streamFile));
 
 // start it
 app.listen(3000);
 console.log("App listening... http://localhost:3000");
+
+// handlers
+var onFinished = require('finished');
+var fs = require('fs');
+var path = require('path');
+var extname = path.extname;
+
+function *streamFile(fileName){
+	var path = __dirname + "/sounds/" + fileName + ".mp3";
+	this.type = extname(path);
+
+	var stream = this.body = fs.createReadStream(path);
+
+	// avoid any possible fd leak
+	onFinished(this, stream.destroy.bind(stream));
+};
