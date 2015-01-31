94bf59196f8c0cba81e1657c17967f699387baec
Marcus Hammarberg <marcusoft.net@gmail.com>
2015-01-31 17:56:24 +0700

    Minor fixes

--- routes/siteRoutes.js
+++ routes/siteRoutes.js
@@ -3,7 +3,9 @@ var config = require('../config')();
 var db = require("../lib/db.js");
 var soundCollection = db.sounds(config.mongoUrl);
 
-module.exports.showDisclaimerPage = function *(){ this.body = yield render('disclaimer');};
+module.exports.showDisclaimerPage = function *(){
+	this.body = yield render('disclaimer');
+};
 
 module.exports.showHomePage = function *(){
 	var soundList = yield soundCollection.find({});
