07fee29493e3de959f8e8f5a0851c375c0b668c2
Marcus Hammarberg <marcusoft.net@gmail.com>
2015-02-04 15:37:07 +0700

    Added button to copy autoplay URL

--- public/js/site.js
+++ public/js/site.js
@@ -5,18 +5,31 @@ function playSound(soundName) {
     $.ajax({
         type: 'POST',
         url: '/api/played/' + soundName,
-        dataType : 'json',
+        dataType: 'json',
         success: function(data) {
-            if(data.noOfPlays === 1){
-                $("#noOfPlays"+soundName).html("Sound played 1 time");
-            }else{
-                $("#noOfPlays"+soundName).html("Sound played " + data.noOfPlays + " times");
+            if (data.noOfPlays === 1) {
+                $("#noOfPlays" + soundName).html("Plays: 1");
+            } else {
+                $("#noOfPlays" + soundName).html("Plays: " + data.noOfPlays);
             }
             console.log(data);
         },
         error: function(e) {
-            console.log("Errors when played '" + soundName + "'");
+            console.log("Errors when playing '" + soundName + "':");
             console.log(e);
         }
     });
 };
+
+var client = new ZeroClipboard(document.getElementById("copy-button"));
+
+client.on("ready", function(readyEvent) {
+    // alert( "ZeroClipboard SWF is ready!" );
+
+    client.on("aftercopy", function(event) {
+        // `this` === `client`
+        // `event.target` === the element that was clicked
+        // event.target.style.display = "none";
+        // alert("Copied text to clipboard: " + event.data["text/plain"]);
+    });
+});
